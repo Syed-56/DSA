24947c7dae9b465cfe7ac556653c77008465cdbd
Syed-56 <ssa.56.sultan@gmail.com>
2025-06-30 12:23:20 +0500

    update

--- Bitwise/single_number.cpp
+++ Bitwise/single_number.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 int singleNumber(vector<int>& nums) {   //STL containers are by-default passed by value so we use & to pass address
-    bool found;
-    for(int i=0; i<nums.size(); i++) {
-        found = false;
-        for(int j=0; j<nums.size(); j++) {
-            if(nums.at(i)==nums.at(j) && i!=j)    found= true;
-        }
-        if(!found)  return nums.at(i);
-    } 
+    int ans = 0;
+    for(int val: nums) {
+        ans ^= val;     //here number is being xored by himself so duplicates xoring themselves will result in 0 and the remaining number xoring with 0 will return itselt
+    }
+    return ans;
 }
 
 int main() {
