8a4d7bca338bc2cf34a5f80913ea8b0b6fa61e4b
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-02 01:18:40 +0500

    solved but unoptimized

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -2,6 +2,23 @@
 #include <vector>
 using namespace std;
 
+int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n==1)    return nums[n-1];
+    int freq;
+    for(int i=0; i<n; i++) {
+        freq=1;
+        for(int j=i+1; j<n; j++) {
+            if(nums[i]==nums[j]) {
+                freq++;
+            }
+        }
+        if(freq > n/2)  return nums[i];
+    }
+    return -1;
+}
+
 int main() {
-    
+    vector<int> nums = {1,3,3,2,3};
+    cout << "Majority Element: " << majorityElement(nums);
 }
