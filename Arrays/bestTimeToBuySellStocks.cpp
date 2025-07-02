3bbdda54cb8b820583e447f020943d69d360d015
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-03 01:37:15 +0500

    solved but unoptimized

--- Arrays/bestTimeToBuySellStocks.cpp
+++ Arrays/bestTimeToBuySellStocks.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int profit(vector<int> nums) {
-    int n = nums.size();
+int profit(vector<int> prices) {
+    int n = prices.size();
+    int maxProfit=0;
+
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            int diff = prices[j]-prices[i];
+            if(diff > maxProfit)  maxProfit = diff;
+        }
+    }
+    return maxProfit;
 }
 
 int main() {
-    vector<int> nums = {1,2,3,5,8};
+    vector<int> nums = {5,4,3,2,1};
     cout << "Profit: " << profit(nums);
 }
