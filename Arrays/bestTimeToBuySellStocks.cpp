19c4b95c25a8412e7c90e661093e10403d6a7a3b
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-03 00:35:24 +0500

    update

--- Arrays/bestTimeToBuySellStocks.cpp
+++ Arrays/bestTimeToBuySellStocks.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 using namespace std;
 
+int profit(vector<int> nums) {
+    int n = nums.size();
+}
+
 int main() {
-    
+    vector<int> nums = {1,2,3,5,8};
+    cout << "Profit: " << profit(nums);
 }
