1f28544e53af41b6cb8ed8547fc7989a5e118b6a
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-04 01:27:05 +0500

    update

---
+++ Arrays/pivotIndex.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int pivotIndex(vector<int> nums) {
+    
+}
+
+int main() {
+    vector<int> nums = {1,2,3,3,0};
+    cout << "Pivot Index = " << pivotIndex(nums);
+}
