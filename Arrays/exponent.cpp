32e1bbac5ce3969076da1952476f570647ca2871
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-03 22:25:15 +0500

    solved but unoptimized

--- Arrays/exponent.cpp
+++ Arrays/exponent.cpp
@@ -2,10 +2,19 @@
 #include <vector>
 using namespace std;
 
-int power(int x, int n) {
+double power(double x, int n) {
+    double ans=1, exp = abs(n);
+    for(int i=0; i<n; i++) {
+        ans *= x;
+    }
+    if(n < 0)   ans = 1/ans;
     
+    
+    return ans;
 }
 
 int main() {
-
+    double base = 4;
+    int exponent = -2;
+    cout << base << " ^ " << exponent << " = " << power(base,exponent);
 }
