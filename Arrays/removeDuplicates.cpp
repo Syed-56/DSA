e33d43e891ed9680a0bd1770bebb470b71b329e8
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-05 15:09:03 +0500

    solved but unoptimized

--- Arrays/removeDuplicates.cpp
+++ Arrays/removeDuplicates.cpp
@@ -2,11 +2,34 @@
 #include <vector>
 using namespace std;
 
-int removeDuplicates(vector<int> nums) {
-    
+int removeDuplicates(vector<int>& nums) {
+    int n = nums.size();
+    int count=n;
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(nums[i] != -99 && nums[i]==nums[j]) {
+                nums[j] = -99;
+                count--;
+            }
+        }
+    }
+    int insertPos = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != -99) {
+            nums[insertPos++] = nums[i];
+        }
+    }
+    while (insertPos < n) {
+        nums[insertPos++] = -99;
+    }
+
+    for(int val:nums) {
+        cout << val << " ";
+    }
+    return count;
 }
 
 int main() {
-    vector<int> arr = {1,1,2,3,4,4,4,5};
-    cout << "Duplicates = " << removeDuplicates(arr);
+    vector<int> arr = {1,1,2};
+    cout << "Unique = " << removeDuplicates(arr);
 }
