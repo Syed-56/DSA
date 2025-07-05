20d8f80c3146c416b5ac89765b056af2635733fe
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-05 17:12:41 +0500

    optimized

--- Arrays/removeDuplicates.cpp
+++ Arrays/removeDuplicates.cpp
@@ -4,23 +4,16 @@ using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
     int n = nums.size();
-    int count=n;
-    for(int i=0; i<n; i++) {
-        for(int j=i+1; j<n; j++) {
-            if(nums[i] != -99 && nums[i]==nums[j]) {
-                nums[j] = -99;
-                count--;
-            }
+    int count=1, pointer=0;     //bcz nums[0] will always be unique
+    for(int i=1; i<n; i++) {
+        if(nums[i] != nums[pointer]) {
+            nums[count] = nums[i];
+            count++;
+            pointer=i;
         }
     }
-    int insertPos = 0;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] != -99) {
-            nums[insertPos++] = nums[i];
-        }
-    }
-    while (insertPos < n) {
-        nums[insertPos++] = -99;
+    for(int i=count; i<n; i++) {
+        nums[i] = INT16_MIN;
     }
 
     for(int val:nums) {
@@ -30,6 +23,6 @@ int removeDuplicates(vector<int>& nums) {
 }
 
 int main() {
-    vector<int> arr = {1,1,2};
+    vector<int> arr = {1,1,2,3,3,3,4,4,5};
     cout << "Unique = " << removeDuplicates(arr);
 }
