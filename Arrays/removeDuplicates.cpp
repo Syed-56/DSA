3fbfc7473bf02211a754239860e6caf2df33a079
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-05 14:30:38 +0500

    update

---
+++ Arrays/removeDuplicates.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int> nums) {
+    
+}
+
+int main() {
+    vector<int> arr = {1,1,2,3,4,4,4,5};
+    cout << "Duplicates = " << removeDuplicates(arr);
+}
