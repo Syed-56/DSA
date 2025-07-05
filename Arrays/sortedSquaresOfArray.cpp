ab9ca42c4aa5c8f4818cec7ca2681c0938d92d4a
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-05 13:24:38 +0500

    optimized

--- Arrays/sortedSquaresOfArray.cpp
+++ Arrays/sortedSquaresOfArray.cpp
@@ -4,15 +4,25 @@
 using namespace std;
 
 vector<int> squareAndSort(vector<int>& nums) {
-    for(int& val : nums) {
-        val *= val;
+    int n = nums.size();
+    int start = 0, end = n-1;
+    vector<int> arrayToReturn(n,0);
+    for(int i=0; start < end; i++) {
+        if(abs(nums[start]) < abs(nums[end])) {
+            arrayToReturn[n-1-i] = nums[end]*nums[end];
+            end--;
+        }
+        else {
+            arrayToReturn[n-1-i] = nums[start]*nums[start];
+            start++;
+        }
+
     }
-    sort(nums.begin(), nums.end());
-    return nums;
+    return arrayToReturn;
 }
 
 int main() {
-    vector<int> arr = {-4,-1,0,3,10};
+    vector<int> arr = {-4,-1,0,3,5,10};
     vector<int> nums = squareAndSort(arr);
     cout << "Squared and Sorted: ";
     for(int val : nums)  cout << val << " ";
