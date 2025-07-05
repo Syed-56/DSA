9bcc6d117ec3aece3081932ea126365a6be09b29
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-05 18:20:33 +0500

    optimized

--- Arrays/removeElement.cpp
+++ Arrays/removeElement.cpp
@@ -2,12 +2,21 @@
 #include <vector>
 using namespace std;
 
-int removeElement(vector<int>& nums, int k) {
-
+vector<int> removeElement(vector<int>& nums, int k) {
+    int insertPos=0;
+    for(int val:nums) {
+        if(val != k) {
+            nums[insertPos++] = val;
+        }
+    }
+    for(int i=insertPos; i<nums.size(); i++)    nums[i] = k;
+    for(int val:nums)   cout << val << " ";
+    return nums;
 }
 
 int main() {
     vector<int> nums = {0,1,2,2,3,4,2};
     int k=3;
-    cout << "Instances of " << k << " removed = " << removeElement(nums,k);
+    cout << "After Removing " << k << " = ";
+    for(int val:nums)   cout << val << " ";
 }
