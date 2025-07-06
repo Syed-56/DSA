de8bf596c9cfe85a32d3bfbe71ffdcdacdf7aee9
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-07 00:38:34 +0500

    update

---
+++ Arrays/productOfArraExceptSelf.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int> nums) {
+
+}
+
+int main() {
+    vector<int> arr = {1,2,3,4};
+    vector<int> nums = productExceptSelf(arr);
+    for(int val:nums)   cout << val << " ";
+}
