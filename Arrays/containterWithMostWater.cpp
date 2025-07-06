334726b373228ac459a8a192c0b1392d471882ce
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-06 23:50:38 +0500

    optimized

--- Arrays/containterWithMostWater.cpp
+++ Arrays/containterWithMostWater.cpp
@@ -2,8 +2,21 @@
 #include <vector>
 using namespace std;
 
-int containerWithMostWater(vector<int> nums) {
-    
+int containerWithMostWater(vector<int> height) {
+    //container = diff b/w indexes * index with samller value
+    int n = height.size();
+    int left=0, right=n-1;
+    int maxContainer=0, container;
+    while(left<right) {
+        container=0;
+        int h = min(height[left],height[right]);
+        int width = right-left;
+        container = width * h;
+        maxContainer = max(container,maxContainer);
+        if(height[right] < height[left]) right--;
+        else    left++;
+    }
+    return maxContainer;
 }
 
 int main() {
