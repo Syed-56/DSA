16b63d69462219168b428e0c910e0f1f74da4991
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-06 20:41:03 +0500

    update

---
+++ Arrays/maximumProductInSubarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maximumProductInSubarray(vector<int>& nums) {
+    
+}
+
+int main() {
+    vector<int> arr = {2,3,-2,4};
+    int maxProduct = maximumProductInSubarray(arr);
+    cout << "Maximum Product = " << maxProduct;
+}
--- Strings/longestSubstringWithoutRepeatingCharacters.cpp
+++ Strings/longestSubstringWithoutRepeatingCharacters.cpp
@@ -6,21 +6,22 @@ using namespace std;
 
 int longestSubstringWithourRepeatingCharacters(string s) {
     int maxLen=0;
-    string substringToPrint;
-    for(int i=0; i<s.length(); i++) {
-        unordered_set<char> seen;
-        string substring = "";
-        for(int j=i; j<s.length(); j++) {
-            if(seen.count(s[j]))    break;
-            seen.insert(s[j]);
-            substring += s[j];
+    string substringToPrint = "";
+    int left=0, right=0;
+    int currentLength;
+    while(right < s.length()) {
+        if (s[left] != s[right]) {
+            substringToPrint += s[right];
+            currentLength++;
         }
-        if((int)substring.length() > maxLen) {
-            maxLen = (int)substring.length();
-            substringToPrint = substring;
+        else {
+            left = right;
+            currentLength=0;
+            maxLen = max(maxLen, int(substringToPrint.length()));
         }
+        right++;
     }
-    cout << substringToPrint << endl;
+    cout << substringToPrint << "\t";
     return maxLen;
 }
 
