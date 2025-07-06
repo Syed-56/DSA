3f584cc849991ccc2ee86bec81b74befbae920e9
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-06 20:47:17 +0500

    solved but unoptimized

--- Arrays/maximumProductInSubarray.cpp
+++ Arrays/maximumProductInSubarray.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int maximumProductInSubarray(vector<int>& nums) {
-    
+    int n = nums.size();
+    int maxProduct = INT16_MIN;
+    for(int i=0; i<n; i++) {
+        int currentProduct = 1;
+        for(int j=i; j<n; j++) {
+            currentProduct *= nums[j];
+            maxProduct = max(currentProduct, maxProduct);
+        }
+    }
+    return maxProduct;
 }
 
 int main() {
