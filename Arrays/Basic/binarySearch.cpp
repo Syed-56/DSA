35afabc158b919e2a27e0e3d6711ec48b9b34db3
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-08 23:38:57 +0500

    update

--- Arrays/Basic/binarySearch.cpp
+++ Arrays/Basic/binarySearch.cpp
@@ -6,6 +6,7 @@ int binarySearch(vector<int> nums, int target) {
     int start=0, end = nums.size()-1;
     while(start <= end) {
         int mid = (start+end)/2;
+        //if start = end = INT_MAX then overflow occur in a test case so to escape that you can use mid = start + (end-start)/2
         if(target > nums[mid]) {
             start = mid+1;
         }
