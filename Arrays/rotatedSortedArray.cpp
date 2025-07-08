584ebd3962e96554ff50aa621436ea2e56cafcbe
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-08 22:31:43 +0500

    solved but unoptimized

---
+++ Arrays/rotatedSortedArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int> nums, int target) {
+    int n = nums.size();
+    for(int i=0; i<n; i++) {
+        if(nums[i]==target)  return i;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {4,5,6,7,0,1,2};
+    int target=0;
+    cout << "Index = " << search(arr,target);
+}
