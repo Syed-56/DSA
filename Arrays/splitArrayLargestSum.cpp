51fdd69a18f0b11214a81d06458175ff166a6087
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-09 21:04:30 +0500

    update

---
+++ Arrays/splitArrayLargestSum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSumMinimized(vector<int> nums, int k) {
+
+}
+
+int main() {
+    vector<int> nums = {7,2,5,10,8};
+    int k=2;
+    cout << "Maximum Sum Minimized = " << maxSumMinimized(nums,k);
+}
