079c7c059d19ef69dc1e4c1342acaed6c0aea9a7
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-09 18:36:03 +0500

    optimized

--- Arrays/peakIndexinMountain.cpp
+++ Arrays/peakIndexinMountain.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int peakIndex(vector<int> arr) {
     int n = arr.size();
-    int peakIndex=0, peakElement = arr[0];
-    for(int i=0; i<n; i++) {
-        if(arr[i] > peakElement) {
-            peakElement = arr[i];
-            peakIndex=i;
+    int start=1, end=n-2;
+    while(start <= end) {
+        int mid = start + (end-start)/2;
+
+        if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
+            return mid;
+        }
+        else if(arr[mid+1] > arr[mid]) {
+            start=mid+1;
+        }
+        else if(arr[mid+1] < arr[mid]) {
+            end=mid;
         }
     }
-    return peakIndex;
 }
 
 int main() {
