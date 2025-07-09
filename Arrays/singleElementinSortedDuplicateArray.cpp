fefa7c7b66d6c5e0b63a0a2f886c49c2ced084ce
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-09 19:27:55 +0500

    update

---
+++ Arrays/singleElementinSortedDuplicateArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int singleElement(vector<int> nums) {
+
+}
+
+int main() {
+    vector<int> nums = {1,1,2,3,3,5,5};
+    cout << "Single Element = " << singleElement(nums);
+}
