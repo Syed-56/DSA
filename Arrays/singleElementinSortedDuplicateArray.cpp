ee38583da023a1fd1029727ab383f546779ea458
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-09 20:45:44 +0500

    optimized

--- Arrays/singleElementinSortedDuplicateArray.cpp
+++ Arrays/singleElementinSortedDuplicateArray.cpp
@@ -8,6 +8,7 @@ int singleElement(vector<int> nums) {
     while(start <= end) {
         int mid = start + (end-start)/2;
         if(mid==0 || mid==n-1)  return nums[mid];
+        if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1])    return nums[mid];
         if(mid%2 == 0) {  //both sides have even elements
             if(nums[mid] == nums[mid+1]) {      //single element exist in right half
                 start = mid+1;
