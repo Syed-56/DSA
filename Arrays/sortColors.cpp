2b695ed3394199767b3b38119508f2b67c7ab75a
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-14 02:38:35 +0500

    solved but unoptimized

--- Arrays/sortColors.cpp
+++ Arrays/sortColors.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 vector<int> sortColor(vector<int> nums) {
-
+    sort(nums.begin(), nums.end());
+    return nums;
 }
 
 int main() {
     vector<int> colors = {2,0,2,1,1,0,1,2,0,0};
-    nums = sortColor(colors);
+    vector<int> nums = sortColor(colors);
     for(int val:nums)   cout << val << " ";
 }
