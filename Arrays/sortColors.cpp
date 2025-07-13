cccfdb0c143b406d1921d39221417c5e2b3140a4
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-14 02:44:53 +0500

    solved but unoptimized

--- Arrays/sortColors.cpp
+++ Arrays/sortColors.cpp
@@ -4,7 +4,17 @@
 using namespace std;
 
 vector<int> sortColor(vector<int> nums) {
-    sort(nums.begin(), nums.end());
+    int zeroCount=0, oneCount=0, twoCount=0;
+    for(int val:nums) {
+        if(val==0)  zeroCount++;
+        else if(val==1)  oneCount++;
+        else if(val==2)  twoCount++;
+    }
+
+    for(int i=0; i<zeroCount; i++)  nums[i] = 0;
+    for(int i=zeroCount; i<zeroCount+oneCount; i++)   nums[i] = 1;
+    for(int i=zeroCount+oneCount; i<nums.size(); i++)    nums[i] = 2;
+
     return nums;
 }
 
