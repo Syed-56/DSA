29372cc5940c0ecf74f8a808d9c0adff97b13854
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-16 00:49:52 +0500

    update

--- Arrays/2D_Arrays/find-Max-Row.cpp
+++ Arrays/2D_Arrays/find-Max-Row.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 vector<int> maxRow(vector<vector <int>> matrix) {
-
+    int sum=0, maxSum = INT16_MIN, maxRowIndex=-1;
+    for(int i=0; i<matrix.size(); i++) {
+        sum=0;
+        for(int j=0; j<matrix[0].size(); j++) {
+            sum += matrix[i][j];
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxRowIndex = i;
+        }
+    }
+    return matrix[maxRowIndex];
 }
 
 int main() {
@@ -11,7 +22,9 @@ int main() {
     for(int i=0; i<3; i++) {
         for(int j=0; j<3; j++) {
             matrix[i][j] = i+j+1;
+            cout << matrix[i][j];
         }
+        cout << endl;
     }
     vector<int> bigRow = maxRow(matrix);
     for(int val:bigRow)   cout << val << " ";
