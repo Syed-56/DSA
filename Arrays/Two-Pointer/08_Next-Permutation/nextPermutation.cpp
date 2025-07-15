7a00736045dbdeeb5e570f257e785c0463c73ef2
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-15 16:53:46 +0500

    update

---
+++ Arrays/Two-Pointer/08_Next-Permutation/nextPermutation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> nextPermutation(vector<int>& nums) {
+
+}
+
+int main() {
+    vector<int> perm = {1,2,3};
+    vector<int> nextPerm = nextPermutation(perm);
+    for(int val:nums)   cout << val << " ";
+}
