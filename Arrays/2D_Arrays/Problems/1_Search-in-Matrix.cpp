2b8190ad56d03208dbc3587e2ef185171879afe2
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-16 03:22:48 +0500

    optimized

--- Arrays/2D_Arrays/Problems/1_Search-in-Matrix.cpp
+++ Arrays/2D_Arrays/Problems/1_Search-in-Matrix.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 bool search(vector<vector<int>> matrix, int target) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
 
+    int index=-1, finalIndex=-1;;
+
+    for(int i=0; i<rows; i++) {
+        if(matrix[i][0] > target && i != 0) {
+            index = i-1;
+            break;
+        }
+    }
+    if (index == -1) index = rows - 1;
+    cout << "Row: " << index;
+
+    bool found=false;
+    for(int i=0; i<cols; i++) {
+        if(matrix[index][i]==target) {
+            finalIndex = i;
+            found=true;
+            break;
+        }
+    }
+    cout << "\nColumn: " << finalIndex << "\t" << matrix[index][finalIndex] << endl;
+
+    return found;
 }
 
 int main() {
@@ -12,11 +36,6 @@ int main() {
         {10,11,16,20},
         {23,30,34,60}
     };
-    for(int i=0; i<2; i++) {
-        for(int j=0; j<3; j++) {
-            matrix[i][j] = i+1;
-        }
-    }  
     bool target =  search(matrix,34);
     cout << "Found:" << target;
 }
