4601d32ec93ab4be526d56fe4a93da47a5433b30
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-16 17:47:10 +0500

    optimized

--- Arrays/2D_Arrays/Problems/1_Search-in-Matrix.cpp
+++ Arrays/2D_Arrays/Problems/1_Search-in-Matrix.cpp
@@ -6,28 +6,17 @@ bool search(vector<vector<int>> matrix, int target) {
     int rows = matrix.size();
     int cols = matrix[0].size();
 
-    int index=-1, finalIndex=-1;;
+    int start=0, end = rows*cols-1;
+    while (start <= end) {
+        int mid = start + (end-start)/2;
+        int rowIndex = mid/cols;
+        int colIndex = mid%cols;
 
-    for(int i=0; i<rows; i++) {
-        if(matrix[i][0] > target && i != 0) {
-            index = i-1;
-            break;
-        }
+        if(matrix[rowIndex][colIndex] > target)  end = mid-1;
+        else if(matrix[rowIndex][colIndex] < target)    start = mid+1;
+        else    return true;
     }
-    if (index == -1) index = rows - 1;
-    cout << "Row: " << index;
-
-    bool found=false;
-    for(int i=0; i<cols; i++) {
-        if(matrix[index][i]==target) {
-            finalIndex = i;
-            found=true;
-            break;
-        }
-    }
-    cout << "\nColumn: " << finalIndex << "\t" << matrix[index][finalIndex] << endl;
-
-    return found;
+    return false;
 }
 
 int main() {
