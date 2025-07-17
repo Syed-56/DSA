e520a0da8226b64b80eb2cea38718bf848b224f0
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-18 04:18:57 +0500

    solved but unoptimized

--- Arrays/2D_Arrays/Problems/4_Find-Missing-Duplicate.cpp
+++ Arrays/2D_Arrays/Problems/4_Find-Missing-Duplicate.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> findMissingandDuplicate(vector<vector<int>> matrix) {
+    vector<int> nums;
+    int rows = matrix.size(), cols = matrix[0].size();
+    
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            nums.push_back(matrix[i][j]);
+        }
+    }
 
+    int repeat, missing;
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    
+    for(int i=0; i<n; i++) {
+        if(nums[i]==nums[i+1]) {
+            repeat = nums[i];
+            break;
+        }
+    }
+    cout << "Repeat: " << repeat << endl;
+
+    int actualSum=0, gotSum = -repeat;
+    for(int i=0; i<n; i++) {
+        actualSum += i+1;
+        gotSum += nums[i];
+    }
+    cout << "Actual Sum: " << actualSum << "\tGot Sum: " << gotSum << endl;
+    missing = actualSum - gotSum;
+    cout << "\nMissing = " << missing << endl;
+
+    for(int i=0; i<n; i++)  nums.pop_back();
+    nums.push_back(repeat);
+    nums.push_back(missing);
+    return nums;
 }
 
 int main() {
