1718a46579819ca53a4df7e576369c60b9bed9bf
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-20 03:18:49 +0500

    optimized

--- Strings/Sliding-Window/02_removeOccurrenceOfSubstring/remove-occurence.cpp
+++ Strings/Sliding-Window/02_removeOccurrenceOfSubstring/remove-occurence.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 string removeOccurrences(string s, string part) {
-    
+    int open=0, close=part.length();
+    while (close <= s.length()) {
+        if(s.substr(open, close-open) == part) {
+            s.erase(open, close - open);
+            open = 0;
+            close = open+part.length();
+        }
+        else {
+            open++;
+            close++;
+        }
+    }
+    return s;
 }
 
 int main() {
