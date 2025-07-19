c5a64dca99fd5c5b66fb8a9aa973dabd4ad48802
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-20 02:59:21 +0500

    update

---
+++ Strings/Sliding-Window/02_removeOccurrenceOfSubstring/remove-occurence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+    
+}
+
+int main() {
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    
+    string result = removeOccurrences(s, part);
+    cout << result << endl; 
+    
+    return 0;
+}
