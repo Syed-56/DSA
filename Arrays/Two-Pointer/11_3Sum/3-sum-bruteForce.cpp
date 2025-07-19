016cad44f8eaa8e3bbed9cac09431d3eb2468316
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-19 15:42:23 +0500

    solved but unoptimized

--- Arrays/Two-Pointer/11_3Sum/3-sum-bruteForce.cpp
+++ Arrays/Two-Pointer/11_3Sum/3-sum-bruteForce.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 using namespace std;
 
-vector<int> threeSum(vector<int> nums) {
+vector<vector <int>> threeSum(vector<int> nums) {
+    int n = nums.size();
+    vector<vector <int>> pairs;
+    set<vector <int>> set;
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            for(int k=j+1; k<n; k++) {
+                if(i==j && j==k && i==k) continue;
+                if(nums[i]+nums[j]+nums[k] == 0) {
+                    vector<int> triplets = {nums[i],nums[j],nums[k]};
+                    sort(triplets.begin(), triplets.end());
 
+                    if(set.find(triplets) == set.end()) {
+                        set.insert(triplets);
+                        pairs.push_back(triplets);
+                    }
+                }
+            }
+        }
+    }
+    return pairs;
 }
 
 int main() {
     vector<int> nums = {-1,0,1,2,-1,-4};
-    vector<int> pairs = threeSum(nums);
+    vector<vector <int>> pairs = threeSum(nums);
     cout << "PAir Indices: ";
-    for(int val:pairs)  cout << val << " ";
+    for(const auto& row:pairs) {
+        for(int val:row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
 }
