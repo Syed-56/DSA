c71a5045aae7d083e892d54e0fe39691c1ed5940
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-20 18:34:39 +0500

    update

---
+++ Strings/Two-Pointer/02_reverse-words-in-string/reverseWordsinString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseWord(string s) {
+
+}
+
+int main() {
+    string s = "Sky is the blue";
+    string result = reverseWord(s);
+    cout << result << endl; 
