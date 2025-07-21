51032e9920b64d75a69d9af6d5d2bfbfcf7f4f9b
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-21 16:27:54 +0500

    update

---
+++ Strings/Two-Pointer/03_string-compression/stringCompression.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int stringCompression(string s) {
+
+}
+
+int main() {
+    string s = "aabbbccd";
+    int compressed = stringCompression(s);
+    cout << "\nCompressed length: " << compressed << endl;
+    return 0;
+}
