2b02fb606bf77407f1d97316116e05f0fc68371f
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-21 16:49:43 +0500

    optimized

--- Strings/Two-Pointer/03_string-compression/stringCompression.cpp
+++ Strings/Two-Pointer/03_string-compression/stringCompression.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int stringCompression(string s) {
-
+int compress(vector<char>& chars) {
+    int n = chars.size();
+    vector<char> ans;
+    for(int i=0; i<n; i++) {
+        int count=1;
+        if(count==1)    ans.push_back(chars[i]);
+        while(i<n-1 && chars[i]==chars[i+1]) {
+            count++;
+            i++;
+        }
+        if (count > 1) {
+            string countStr = to_string(count);
+            for (char c : countStr) {
+                ans.push_back(c);
+            }
+        }
+    }
+    
+    chars = ans; // Update the original vector with compressed characters
+    return ans.size();
 }
 
 int main() {
-    string s = "aabbbccd";
-    int compressed = stringCompression(s);
+    vector<char> s = {'a', 'a', 'b', 'b','c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c'};
+    int compressed = compress(s);
     cout << "\nCompressed length: " << compressed << endl;
     return 0;
 }
