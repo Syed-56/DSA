847da534d648c189cdd601b92721e4d4baea2bfb
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-21 23:51:53 +0500

    solved but unoptimized

---
+++ Strings/Sliding-Window/01_longest-unique-substring/longestSubstringWithoutRepeatingCharacters-optimized.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    
+}
+
+int main() {
+    string s = "abcabcbb";
+    int length = lengthOfLongestSubstring(s);
+    cout << "Length of the longest substring without repeating characters: " << length << endl;
+}
