34c9cf5b109fd26f5b13763b20af06a5c57f2eb7
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-21 16:09:26 +0500

    optimized

--- Strings/Two-Pointer/02_reverse-words-in-string/reverseWordsinString.cpp
+++ Strings/Two-Pointer/02_reverse-words-in-string/reverseWordsinString.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 string reverseWord(string s) {
+    int n = s.length();
+    int letter = 0;
+    string ans = "";
+    reverse(s.begin(), s.end());
 
+    while(letter < n) {
+        if(s[letter] == ' ') {
+            letter++;
+            continue;
+        }
+        string word = "";
+        while(s[letter] != ' ' && letter < n) {
+            word += s[letter];
+            letter++;
+        }
+        reverse(word.begin(), word.end());
+        if(word.length()>0) ans += word + " ";
+    }
+    while(ans[ans.length() - 1] == ' ') {
+        ans.pop_back();
+    }
+    return ans;
 }
 
 int main() {
-    string s = "Sky is the blue";
+    string s = "  Sky is the blue  ";
     string result = reverseWord(s);
-    cout << result << endl; 
+    cout << "|" << result << "|"; 
 }
