75400fd4072844e5f367059941f054cad3e20c7f
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-23 03:31:39 +0500

    optimized

--- Linked-List/Basic-Traversing/01_Reverse-Linked-List/optimize.cpp
+++ Linked-List/Basic-Traversing/01_Reverse-Linked-List/optimize.cpp
@@ -9,7 +9,18 @@ struct ListNode {
 
 // Reverse a singly linked list
 ListNode* reverseList(ListNode* head) {
-    
+    ListNode* prev = NULL;
+    ListNode* Next = NULL;
+    ListNode* curr = head;
+
+    while(curr != NULL) {
+        Next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+    }
+
+    return prev;
 }
 
 void printList(ListNode* head) {
