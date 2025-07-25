9d7b5d892185d2713bc45d5689bd411861a32868
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-25 17:46:00 +0500

    update

---
+++ Linked-List/Recursion/03_reverse-nodes-in-k-group/optimize.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// Reverse a singly linked list
+ListNode* reverseList(ListNode* head, int k) {
+    
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL\n";
+}
+
+int main() {
+    // Build list: 10 -> 20 -> 30 -> ...
+    ListNode* head = new ListNode(10);
+    head->next = new ListNode(20);
+    head->next->next = new ListNode(30);
+    head->next->next->next = new ListNode(40);
+    head->next->next->next->next = new ListNode(50);
+
+    head = reverseList(head, 2);
+
+    printList(head);
+}
