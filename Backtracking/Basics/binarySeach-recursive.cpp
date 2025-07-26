d883c0fb8de106949e950d010b6eadc0a609cfeb
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 01:14:52 +0500

    optimized

---
+++ Backtracking/Basics/binarySeach-recursive.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int target, int start, int end) {
+    if(start > end)     return -1;
+    int mid = start+end/2;
+    if(arr[mid] > target)       return binarySearch(arr,target,start,end-1);
+    else if(arr[mid] < target)  return binarySearch(arr,target,start+1,end);
+    else    return mid;
+}
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int target=3;
+    cout << "Target Found at Index: " << binarySearch(arr,target,0,5-1);
+}
