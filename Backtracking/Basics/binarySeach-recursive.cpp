d5af1918abe7db9bd4966e858c2d05154325be98
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 01:22:26 +0500

    optimized

--- Backtracking/Basics/binarySeach-recursive.cpp
+++ Backtracking/Basics/binarySeach-recursive.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int binarySearch(int arr[], int target, int start, int end) {
     if(start > end)     return -1;
     int mid = start+end/2;
-    if(arr[mid] > target)       return binarySearch(arr,target,start,end-1);
-    else if(arr[mid] < target)  return binarySearch(arr,target,start+1,end);
+    if(arr[mid] > target)       return binarySearch(arr,target,start,mid-1);
+    else if(arr[mid] < target)  return binarySearch(arr,target,mid+1,end);
     else    return mid;
 }
 
