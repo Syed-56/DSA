26b83f519345e3e0fb4253ef8c23fe225e811a41
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 00:27:47 +0500

    update

---
+++ Backtracking/Basics/fibonaci.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+    if(n==0 || n==1)    return n;
+    else    return fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+    int n = 6;
+    int fibonaci = fibo(6);
+    cout << "Fibonacci Series term " << n << " = " << fibonaci;
+}
