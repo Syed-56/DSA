12fe84957738a527c9d3d5689e35faf64a097fdd
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 00:38:27 +0500

    optimized

--- Backtracking/Basics/fibonaci.cpp
+++ Backtracking/Basics/fibonaci.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int fibo(int n) {
     if(n==0 || n==1)    return n;
-    else    return fibo(n-1) + fibo(n-2);
+    else                return fibo(n-1) + fibo(n-2);
 }
 
 int main() {
