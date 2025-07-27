5138874d1f3ce71b03eba95f3797b7bf119643b3
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 19:21:32 +0500

    optimized

--- Backtracking/Easy-Problems/01_subsets/optimize.cpp
+++ Backtracking/Easy-Problems/01_subsets/optimize.cpp
@@ -3,18 +3,31 @@
 using namespace std;
 
 void getSubsets(vector<int>& nums, vector<int> subset, vector<vector<int>> &ans, int i) {
-    if(i==nums.size()) {
+    // Print the current index and subset
+    cout << "Called getSubsets(i = " << i << ", subset = [";
+    for (int val : subset) cout << val << " ";
+    cout << "])" << endl;
+
+    // Base case: if we've reached the end of the array
+    if (i == nums.size()) {
+        cout << "-> Reached end. Saving subset: [";
+        for (int val : subset) cout << val << " ";
+        cout << "]" << endl;
         ans.push_back(subset);
         return;
     }
+
+    // Include nums[i] in subset
+    cout << "Including nums[" << i << "] = " << nums[i] << endl;
     subset.push_back(nums[i]);
-    getSubsets(nums,subset,ans,i+1);
-    cout << "Current Subset: ";
-    for(int val:subset) cout << val;
-    cout << endl;
+    getSubsets(nums, subset, ans, i + 1);
+
+    // Backtrack: remove nums[i] and try without it
+    cout << "Excluding nums[" << i << "] = " << nums[i] << " after backtracking" << endl;
     subset.pop_back();
-    getSubsets(nums,subset,ans,i+1);
+    getSubsets(nums, subset, ans, i + 1);
 }
+
 
 vector<vector<int>> subsets(vector<int> &nums) {
     vector<vector<int>> allSubsets;
