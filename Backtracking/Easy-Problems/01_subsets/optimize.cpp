1648b2e381962e306eb7821ade1ddd713c622fcc
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-27 19:10:15 +0500

    optimized

---
+++ Backtracking/Easy-Problems/01_subsets/optimize.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void getSubsets(vector<int>& nums, vector<int> subset, vector<vector<int>> &ans, int i) {
+    if(i==nums.size()) {
+        ans.push_back(subset);
+        return;
+    }
+    subset.push_back(nums[i]);
+    getSubsets(nums,subset,ans,i+1);
+    subset.pop_back();
+    getSubsets(nums,subset,ans,i+1);
+}
+
+vector<vector<int>> subsets(vector<int> &nums) {
+    vector<vector<int>> allSubsets;
+    vector<int> subset;
+    getSubsets(nums,subset,allSubsets,0);
+    return allSubsets;
+}
+
+int main() {
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> sub = subsets(nums);
+    for(const auto& row:sub) {
+        for(int val:row) {
+            cout << val;
+        }
+        cout << "  ";
+    }
+}
