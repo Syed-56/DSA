0ba15095f3e9b2f423de59f1427c7b1e1dd19451
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-03 04:30:15 +0500

    optimized

--- Backtracking/Medium-Problems/02_Combination-Sum-II/optimize.cpp
+++ Backtracking/Medium-Problems/02_Combination-Sum-II/optimize.cpp
@@ -5,38 +5,35 @@
 using namespace std;
 
 set<vector<int>> s;
-void backTrack(vector<int> nums, int i, vector<int>& combination, vector<vector<int>>& ans, int target) {
-    if(i==nums.size()) {
-        return;
-    }
-    if(target==0) {
-       if(s.find(combination) == s.end()) {
-            s.insert(combination);
-            ans.push_back(combination);
+    void backTrack(vector<int>& nums, int i, vector<int>& combination, vector<vector<int>>& ans, int target) {
+        if(target==0) {
+            vector<int> sortedComb = combination;
+            sort(sortedComb.begin(), sortedComb.end());
+            if(s.find(sortedComb) == s.end()) {
+                s.insert(sortedComb);
+                ans.push_back(sortedComb);
+            }
+            return;
         }
-        return;
+        if (target < 0 || i >= int(nums.size())) return;
+        combination.push_back(nums[i]);
+        backTrack(nums,i+1,combination,ans,target-nums[i]); //include element once
+        combination.pop_back(); //exclude element while backtracking
+        int next=i+1;
+        while(next<int(nums.size()) && nums[next]==nums[i]) {
+            next++;
+        }
+        backTrack(nums,next,combination,ans,target); //since we dont include a number so we dont decrease our target
     }
-    if(target<0) {
-        return;
-    }
-    combination.push_back(nums[i]);
-    backTrack(nums,i+1,combination,ans,target-nums[i]); //include element once
-    combination.pop_back(); //exclude element while backtracking
-    int next=i+1;
-    while(i>0 && nums[i-1]==nums[i]) {
-        next++;
-    }
-    backTrack(nums,next,combination,ans,target); //since we dont include a number so we dont decrease our target
-}
 
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    vector<int> combination;
-    vector<vector<int>> ans;
-    sort(candidates.begin(), candidates.end());
-    backTrack(candidates,0,combination,ans,target);
-    sort(ans.begin(), ans.end());
-    return ans;
-}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> combination;
+        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        backTrack(candidates,0,combination,ans,target);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
 
 int main() {
     vector<int> nums = {2,3,5};
