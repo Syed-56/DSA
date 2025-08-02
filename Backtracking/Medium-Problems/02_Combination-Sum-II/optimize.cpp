02ff3eba021f452a2a0f942ec6b6c47aa6b50ea2
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-03 04:28:21 +0500

    optimized

--- Backtracking/Medium-Problems/02_Combination-Sum-II/optimize.cpp
+++ Backtracking/Medium-Problems/02_Combination-Sum-II/optimize.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <set>
 using namespace std;
 
@@ -9,7 +10,7 @@ void backTrack(vector<int> nums, int i, vector<int>& combination, vector<vector<
         return;
     }
     if(target==0) {
-        if(s.find(combination) == s.end()) {
+       if(s.find(combination) == s.end()) {
             s.insert(combination);
             ans.push_back(combination);
         }
@@ -18,24 +19,28 @@ void backTrack(vector<int> nums, int i, vector<int>& combination, vector<vector<
     if(target<0) {
         return;
     }
-
     combination.push_back(nums[i]);
     backTrack(nums,i+1,combination,ans,target-nums[i]); //include element once
-    backTrack(nums,i,combination,ans,target-nums[i]);   //include elements multiple times
     combination.pop_back(); //exclude element while backtracking
-    backTrack(nums,i+1,combination,ans,target); //since we dont include a number so we dont decrease our target
+    int next=i+1;
+    while(i>0 && nums[i-1]==nums[i]) {
+        next++;
+    }
+    backTrack(nums,next,combination,ans,target); //since we dont include a number so we dont decrease our target
 }
 
-vector<vector<int>> combinationSum(vector<int>& candidate, int target) {
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
     vector<int> combination;
     vector<vector<int>> ans;
-    backTrack(candidate,0,combination,ans,target);
+    sort(candidates.begin(), candidates.end());
+    backTrack(candidates,0,combination,ans,target);
+    sort(ans.begin(), ans.end());
     return ans;
 }
 
 int main() {
     vector<int> nums = {2,3,5};
-    vector<vector<int>> sumsPair = combinationSum(nums,8);
+    vector<vector<int>> sumsPair = combinationSum2(nums,8);
     for(const auto& row:sumsPair) {
         for(int val:row) {
             cout << val << " ";
