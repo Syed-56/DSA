113316e969a41d7105548603b5cc04422ce19f88
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-03 09:47:51 +0500

    update

--- Backtracking/Medium-Problems/05_Quick-Merge-Sort/merge-sort.cpp
+++ Backtracking/Medium-Problems/05_Quick-Merge-Sort/merge-sort.cpp
@@ -2,8 +2,40 @@
 #include <vector>
 using namespace std;
 
-void mergeSort(vector<int>& nums, int start, int end) {
+void merge(vector<int>& nums, int start, int mid, int end) {
+    int i=start, j=mid+1;
+    vector<int> temp;
+    while(i <= mid && j<= end) {
+        if(nums[i] <= nums[j]) {
+            temp.push_back(nums[i]);
+            i++;
+        }
+        else {
+            temp.push_back(nums[j]);
+            j++;
+        }
+    }
 
+    while(i<=mid) {
+        temp.push_back(nums[i]);
+        i++;
+    }
+    while(j<=end) {
+        temp.push_back(nums[j]);
+        j++;
+    }
+
+    for(int idx=0; idx<temp.size(); idx++) {
+        nums[idx+start] = temp[idx];
+    }
+}
+
+void mergeSort(vector<int>& nums, int start, int end) {
+    if(start >= end)  return;
+    int mid = start + (end-start)/2;
+    mergeSort(nums,start,mid);  //Sort Left Half
+    mergeSort(nums,mid+1,end);  //Sort Right Half
+    merge(nums,start,mid,end);  //merge sorted halves
 }
 
 int main() {
