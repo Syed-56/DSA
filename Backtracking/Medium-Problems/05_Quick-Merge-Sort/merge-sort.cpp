8f92f8981f12c17788247b3b0e4880f99427440d
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-03 07:23:08 +0500

    update

---
+++ Backtracking/Medium-Problems/05_Quick-Merge-Sort/merge-sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void mergeSort(vector<int>& nums, int start, int end) {
+
+}
+
+int main() {
+    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+    int n = arr.size()-1;
+    mergeSort(arr,0,n);
+    for(int val:arr)    cout << val << " ";
+}
