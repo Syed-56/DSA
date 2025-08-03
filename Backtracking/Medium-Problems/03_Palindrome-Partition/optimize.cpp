32cfe023b494916e4ccb233ecdfe1f07cbebe185
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-03 05:17:58 +0500

    optimized

--- Backtracking/Medium-Problems/03_Palindrome-Partition/optimize.cpp
+++ Backtracking/Medium-Problems/03_Palindrome-Partition/optimize.cpp
@@ -3,9 +3,29 @@
 #include <string>
 using namespace std;
 
-void backTrack(string s, vector<vector<string>>& ans, vector<string>& combination, int i) {
-    if(i==s.size())     return;
-    
+bool isPalindrome(string s) {
+    int start=0, end = s.size()-1;
+    while(start < end) {
+        if(s[start] != s[end])  return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+void backTrack(string s, vector<vector<string>>& ans, vector<string>& combination, int call) {
+    if(call==s.size()) {
+        ans.push_back(combination);
+        return;
+    }
+
+    for(int i=call; i<s.size(); i++) {
+        if(isPalindrome(s.substr(call,i-call+1))) {
+            combination.push_back(s.substr(call,i-call+1));
+            backTrack(s,ans,combination,i+1);   //now find palindrome from string after current letter
+            combination.pop_back(); //backtrack
+        }
+    }
 }
 
 vector<vector<string>> partition(string s) {
